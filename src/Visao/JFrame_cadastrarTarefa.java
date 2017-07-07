e8f3217ff8c1754bfbaab71ed776f446c9825c5d
Anderson <andsonp2@gmail.com>
2017-07-07 04:49:02 -0300

    telas de professor e aluno

--- src/Visao/JFrame_cadastrarTarefa.java
+++ src/Visao/JFrame_cadastrarTarefa.java
@@ -5,6 +5,9 @@
  */
 package Visao;
 
+import javax.swing.text.DefaultFormatterFactory;
+import javax.swing.text.MaskFormatter;
+
 /**
  *
  * @author Anderson
@@ -28,6 +31,11 @@ public class JFrame_cadastrarTarefa extends javax.swing.JFrame {
     private void initComponents() {
 
         jL_bemvindoAluno = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jT_nomealunoAlterarAluno = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(234, 234, 234));
@@ -42,6 +50,31 @@ public class JFrame_cadastrarTarefa extends javax.swing.JFrame {
         getContentPane().add(jL_bemvindoAluno);
         jL_bemvindoAluno.setBounds(0, 0, 660, 40);
 
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(40, 110, 590, 190);
+
+        jLabel5.setFont(new java.awt.Font("Agency FB", 0, 22)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(36, 46, 68));
+        jLabel5.setText("Prazo de entrega:");
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(50, 320, 130, 27);
+
+        jLabel6.setFont(new java.awt.Font("Agency FB", 0, 22)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(36, 46, 68));
+        jLabel6.setText("Tarefa:");
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(40, 80, 50, 27);
+
+        jT_nomealunoAlterarAluno.setFont(new java.awt.Font("Agency FB", 0, 20)); // NOI18N
+        jT_nomealunoAlterarAluno.setForeground(new java.awt.Color(36, 44, 68));
+        jT_nomealunoAlterarAluno.setText(" ");
+        getContentPane().add(jT_nomealunoAlterarAluno);
+        jT_nomealunoAlterarAluno.setBounds(180, 320, 120, 30);
+
         setSize(new java.awt.Dimension(671, 449));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
@@ -54,7 +87,9 @@ public class JFrame_cadastrarTarefa extends javax.swing.JFrame {
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+       
          */
+       
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -72,7 +107,7 @@ public class JFrame_cadastrarTarefa extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(JFrame_cadastrarTarefa.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+            
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -83,5 +118,11 @@ public class JFrame_cadastrarTarefa extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jL_bemvindoAluno;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jT_nomealunoAlterarAluno;
+    private javax.swing.JTextArea jTextArea1;
     // End of variables declaration//GEN-END:variables
+
 }
