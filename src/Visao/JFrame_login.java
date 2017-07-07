d1f89e21abe42f56fe954390705eacab1cb88021
Anderson <andsonp2@gmail.com>
2017-07-07 01:47:43 -0300

    login

--- src/Visao/JFrame_login.java
+++ src/Visao/JFrame_login.java
@@ -5,9 +5,13 @@
  */
 package Visao;
 
+import Controle.AdmDAO;
 import Controle.AlunoDAO;
 import Controle.Conexao;
+import Controle.ProfessorDAO;
+import Modelo.Administrador;
 import Modelo.Aluno;
+import Modelo.Professor;
 import Modelo.Usuario;
 import java.sql.SQLException;
 import java.util.logging.Level;
@@ -22,7 +26,11 @@ import javax.swing.JOptionPane;
 public class JFrame_login extends javax.swing.JFrame {
     
     Aluno model = new Aluno();
-    AlunoDAO control = new AlunoDAO();
+    Professor model_professor = new Professor();
+    Administrador model_adm = new Administrador();
+    AlunoDAO control_aluno = new AlunoDAO();
+    AdmDAO control_adm = new AdmDAO();
+    ProfessorDAO control_professor = new ProfessorDAO();
     /**
      * Creates new form login
      */
@@ -41,16 +49,21 @@ public class JFrame_login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
         icon = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jBtn_entrar = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
         jT_usuario = new javax.swing.JTextField();
         jT_senha = new javax.swing.JPasswordField();
-        jButton1 = new javax.swing.JButton();
+        jR_adm = new javax.swing.JRadioButton();
+        jR_prof = new javax.swing.JRadioButton();
+        jR_aluno = new javax.swing.JRadioButton();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login");
@@ -112,17 +125,6 @@ public class JFrame_login extends javax.swing.JFrame {
         });
         getContentPane().add(jBtn_entrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 310, -1, -1));
 
-        jButton2.setFont(new java.awt.Font("Agency FB", 0, 14)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(36, 46, 68));
-        jButton2.setText("Esqueci a senha");
-        jButton2.setContentAreaFilled(false);
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 320, -1, -1));
-
         jT_usuario.setFont(new java.awt.Font("Agency FB", 0, 20)); // NOI18N
         jT_usuario.setForeground(new java.awt.Color(36, 46, 68));
         jT_usuario.setOpaque(false);
@@ -133,23 +135,48 @@ public class JFrame_login extends javax.swing.JFrame {
         jT_senha.setOpaque(false);
         getContentPane().add(jT_senha, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 240, 380, 30));
 
-        jButton1.setBackground(new java.awt.Color(228, 228, 228));
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/imagens/login_user.fw.png"))); // NOI18N
-        jButton1.setBorder(null);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 80, -1, -1));
+        buttonGroup1.add(jR_adm);
+        getContentPane().add(jR_adm, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 100, -1, -1));
+
+        buttonGroup1.add(jR_prof);
+        getContentPane().add(jR_prof, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 100, -1, -1));
+
+        buttonGroup1.add(jR_aluno);
+        getContentPane().add(jR_aluno, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 100, -1, -1));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/imagens/login_prof.fw.png"))); // NOI18N
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(560, 80, -1, -1));
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/imagens/login_user.fw.png"))); // NOI18N
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 80, -1, -1));
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/imagens/icon_professor.fw.png"))); // NOI18N
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 80, -1, -1));
     }// </editor-fold>//GEN-END:initComponents
 
     private void jBtn_entrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtn_entrarActionPerformed
-    if((jT_usuario.getText()!="") || (jT_senha.getText()!="")){
+   int tipo_user;
+        if((jT_usuario.getText()!="") || (jT_senha.getText()!="")){
+        //Atribui os  valores dos campos para as variaveis
         int mat =(Integer.parseInt(jT_usuario.getText()));
         String senha = (jT_senha.getText());
+        
+        //Verifica o tipo de usuário
+        if(jR_adm.isSelected()){
+            tipo_user = 1;
+           // int a = control_adm.loginAdm(mat, senha);
+        }
+        else if(jR_prof.isSelected()){
+            tipo_user = 2;
+            //int a = control_professor.loginProfessor(mat, senha);
+        }
+        else if(jR_aluno.isSelected()){
+            tipo_user = 3;
+            int a = control_aluno.loginAluno(mat, senha);
+        }
+                
         try {
-            int a = control.loginAluno(mat, senha);
+            int a = control_aluno.loginAluno(mat, senha);
                 JOptionPane.showMessageDialog(null, "Essa porra existe "+a);
         } catch (SQLException ex) {
             Logger.getLogger(JFrame_login.class.getName()).log(Level.SEVERE, null, ex);
@@ -161,14 +188,6 @@ public class JFrame_login extends javax.swing.JFrame {
     }
 
     }//GEN-LAST:event_jBtn_entrarActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -207,14 +226,19 @@ public class JFrame_login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel icon;
     private javax.swing.JButton jBtn_entrar;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JRadioButton jR_adm;
+    private javax.swing.JRadioButton jR_aluno;
+    private javax.swing.JRadioButton jR_prof;
     private javax.swing.JPasswordField jT_senha;
     private javax.swing.JTextField jT_usuario;
     // End of variables declaration//GEN-END:variables
